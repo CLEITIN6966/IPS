d00d1e18631b71f657522595a658229e54c82011
CLEITIN6966 <cleitinholeiti@gmail.com>
2025-07-19 21:06:25 -0300

    Create kick.lua

---
+++ kick.lua
@@ -0,0 +1,4 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+player:Kick("⚠️IP LOGGER URL:https://gist.github.com/CLEITIN6966/7d4bf1159dc42275739885b731579dfc") -- Mensagem opcional
